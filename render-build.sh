3e9c7cfb0f4361dc2e0c8b8607970d9c1be60425
jorygut <118095034+jorygut@users.noreply.github.com>
2024-12-17 15:12:17 -0800

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,10 @@
+#!/usr/bin/env bash
+
+# Update package list
 apt-get update
+
+# Install Tesseract OCR and its dependencies
 apt-get install -y tesseract-ocr libtesseract-dev
+
+# Install Python dependencies
+pip install -r requirements.txt
